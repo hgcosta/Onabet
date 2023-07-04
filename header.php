a20b95122db3209004146bd5cb5022e0887efa03
hgcosta <hugocosta.devs@gmail.com>
2023-07-04 18:49:29 -0300

    feature: styles header

--- header.php
+++ header.php
@@ -4,12 +4,15 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/fav.png">
-        <meta name="keywords" content="Tom Luzir, Música, Recife, PE">
+        <meta name="keywords" content="">
         <meta name="description" content="">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <meta name="author" content="Jazzz Agência Digital | Hugo Costa">
+        <meta name="author" content="Hugo Costa">
         <meta property="og:site_name" content=""/>
         <!-- Bootstrap-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/owl.carousel.min.css">
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/owl.theme.default.min.css">
@@ -23,9 +26,9 @@
     </head>
     <body <?php body_class()?> >
     <header>
-        <div class="container">
+        <div class="container content-header">
             <div class="logo">
-               <a href=""><img src="" alt=""></a>
+               <a href="" title="Onabet"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/logo-onabet.svg" alt="Logo Onabet"></a>
             </div>
             <nav>
                 <ul class="list-unstyled">
@@ -35,5 +38,10 @@
                     <li><a href="">Quero ser afiliado</a></li>
                 </ul>
             </nav>
+        </div>
+        <div class="container">
+            <div class="divider">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/divider.png" alt="Divider">
+            </div>
         </div>
     </header>
